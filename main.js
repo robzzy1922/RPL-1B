3977482be9d4d028e3b022b65ceaf51a4a85743e
robzzy1922 <robipermanaofficial@gmail.com>
2024-01-14 11:00:25 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function closeNavbar() {
+  var navbarToggle = document.querySelector(".navbar-toggler");
+  var navbarCollapse = document.querySelector(".navbar-collapse");
+  navbarCollapse.classList.remove("show");
+}

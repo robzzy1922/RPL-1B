b56125692a3a921972a71852abaeab4d58917c5e
robzzy1922 <robipermanaofficial@gmail.com>
2024-01-16 13:08:05 +0700

    first commit

--- main.js
+++ main.js
@@ -17,3 +17,80 @@ document.addEventListener("DOMContentLoaded", function () {
     cursor.style.top = mouseY + "px";
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "image",
+        image: {
+          src: "img/partikel.png", // Ganti dengan path gambar hujan salju Anda
+          width: 20,
+          height: 20,
+        },
+      },
+      opacity: {
+        value: 0.8,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 10,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: false,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "bottom",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: false,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: false,
+          mode: "push",
+        },
+        resize: true,
+      },
+    },
+    retina_detect: true,
+  });
+});
--- pascal.js
+++ pascal.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const images = ["img/fauzi-1.jpg", "img/fauzi-2.jpg"];
+  const images = ["img/pascal-1.jpg", "img/pascal-2.jpg"];
   const sliderImg = document.querySelector(".slider-img");
   let currentImageIndex = 0;
 
@@ -17,11 +17,11 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   setInterval(nextImage, 3000); // Auto transition every 3 seconds
-
-  var errorMessage = document.getElementById("errorMessage");
-
-  // Setelah 5 detik, atur opacity tulisan menjadi 0
-  setTimeout(function () {
-    errorMessage.style.opacity = 0;
-  }, 5000);
 });
+
+var errorMessage = document.getElementById("errorMessage");
+
+// Setelah 5 detik, atur opacity tulisan menjadi 0
+setTimeout(function () {
+  errorMessage.style.opacity = 0;
+}, 5000);
